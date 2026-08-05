fdbab724220723d5f9d3dbc3b2bfd098a4aca7bf
StephannyBorrozzino <154994245+StephannyBorrozzino@users.noreply.github.com>
2026-08-05 19:13:41 -0300

    Adiciona projeto Next corujateca

---
+++ corujateca/src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "CorujaTeca",
+  description: "O projeto tem como objetivo melhorar a gestão e o controle de livros, frequentadores e empréstimos em bibliotecas físicas.",
+};
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+  return (
+    <html
+      lang="pt-br"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        
+        {children}
+        
+      </body>
+    </html>
+  );
+}
